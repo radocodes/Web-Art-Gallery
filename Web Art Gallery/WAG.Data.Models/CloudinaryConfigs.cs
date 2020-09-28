57acca32467e7d61d5d1345e2409b9b43415c7de
Radoslav Yordanov <yordanov1234@gmail.com>
2020-09-28 11:55:32 +0300

    Created CloudinaryConfigs data-model

---
+++ Web Art Gallery/WAG.Data.Models/CloudinaryConfigs.cs
@@ -0,0 +1,11 @@
+﻿namespace WAG.Data.Models
+{
+    public class CloudinaryConfigs
+    {
+        public string CloudName { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string ApiSecret { get; set; }
+    }
+}
