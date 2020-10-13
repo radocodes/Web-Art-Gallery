a07d2a12d6adca9cbcbbd1d24c60fa92c81adb1d
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-13 18:44:28 +0300

    Updated WAGDomainAddress.cs for localhost const

--- Web Art Gallery/WAG.Services/Constants/WAGDomainAddress.cs
+++ Web Art Gallery/WAG.Services/Constants/WAGDomainAddress.cs
@@ -2,7 +2,7 @@
 {
     public class WAGDomainAddress
     {
-        public const string LocalHost = "https://localhost:5001";
+        public const string LocalHost = "http://localhost:5001";
         public const string ProductionAddress = "http://emaivanova.com";
     }
 }
