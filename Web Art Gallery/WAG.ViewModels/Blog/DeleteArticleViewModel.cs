6c3f5253c3d8dadb6eb0b0e6739eb75c4daa1282
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-17 12:55:40 +0300

    Updated DeleteArticleViewModel.cs

--- Web Art Gallery/WAG.ViewModels/Blog/DeleteArticleViewModel.cs
+++ Web Art Gallery/WAG.ViewModels/Blog/DeleteArticleViewModel.cs
@@ -5,7 +5,11 @@ namespace WAG.ViewModels.Blog
 {
     public class DeleteArticleViewModel
     {
-        public Article Article { get; set; }
+        public string ArticleId { get; set; }
+
+        public string ArticleTitle { get; set; }
+
+        public string MainPictureFileName { get; set; }
 
         public Cloudinary Cloudinary { get; set; }
     }
