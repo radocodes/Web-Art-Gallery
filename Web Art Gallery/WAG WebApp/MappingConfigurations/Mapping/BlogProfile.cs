f9173c3bbdc4a57140a71adc666bb7d20f6ffb5d
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-17 12:52:49 +0300

    Added BlogProfile.cs (as automapper profile)

---
+++ Web Art Gallery/WAG WebApp/MappingConfigurations/Mapping/BlogProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using WAG.Data.Models;
+using WAG.ViewModels.Blog;
+
+namespace WAG.WebApp.MappingConfigurations.Mapping
+{
+    public class BlogProfile : Profile
+    {
+        public BlogProfile()
+        {
+            this.CreateMap<Article, ArticleDetailsViewModel>()
+                .ForMember(dest => dest.ArticleId, opt => opt.MapFrom(src => src.Id));
+
+            this.CreateMap<CreateArticleViewModel, Article>();
+
+            this.CreateMap<Article, EditArticleViewModel>()
+                .ForMember(dest => dest.ArticleId, opt => opt.MapFrom(src => src.Id));
+
+            this.CreateMap<EditArticleViewModel, Article>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.ArticleId));
+
+            this.CreateMap<Article, DeleteArticleViewModel>()
+                .ForMember(dest => dest.ArticleId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.ArticleTitle, opt => opt.MapFrom(src => src.Title));
+        }
+        
+    }
+}
