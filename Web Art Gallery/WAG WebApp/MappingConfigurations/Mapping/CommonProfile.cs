8c4d42d47bbe4251a58fd63161c36bed3a39d763
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-17 14:49:48 +0300

    Added CommonProfile and UserAccountProfile (as automapper profiles)

---
+++ Web Art Gallery/WAG WebApp/MappingConfigurations/Mapping/CommonProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WAG.Data.Models;
+using WAG.ViewModels.Home;
+
+namespace WAG.WebApp.MappingConfigurations.Mapping
+{
+    public class CommonProfile : Profile
+    {
+        public CommonProfile()
+        {
+            this.CreateMap<ContactMessageViewModel, ContactMessage>();
+        }
+    }
+}
---
+++ Web Art Gallery/WAG WebApp/MappingConfigurations/Mapping/UserAccountProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WAG.Data.Models;
+using WAG.ViewModels.UserAccount;
+
+namespace WAG.WebApp.MappingConfigurations.Mapping
+{
+    public class UserAccountProfile : Profile
+    {
+        public UserAccountProfile()
+        {
+            this.CreateMap<WAGUser, UserDetailsViewModel>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id));
+
+            this.CreateMap<RegisterInputViewModel, WAGUser>();
+
+            this.CreateMap<WAGUser, EditUserProfileViewModel>();
+
+        }
+    }
+}
