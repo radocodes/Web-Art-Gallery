9208c3109de1b820d3462084e3b00d252c51c645
Radoslav Yordanov <yordanov1234@gmail.com>
2020-10-17 14:50:43 +0300

    Disabled HomeController and UserAccountController at BG area

--- Web Art Gallery/WAG WebApp/Areas/BG/Controllers/HomeController.cs
+++ Web Art Gallery/WAG WebApp/Areas/BG/Controllers/HomeController.cs
@@ -2,85 +2,88 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WAG.Data.Models;
 using WAG.Services.Interfaces;
 using WAG.ViewModels.Home;
 using WAG_WebApp.Models;
 
 namespace WAG.WebApp.Areas.BG.Controllers
 {
-    public class HomeController : BGController
-    {
-        private IHomeService HomeService;
-        private IUserAccountService UserAccountService;
+    //public class HomeController : BGController
+    //{
+    //    private IHomeService HomeService;
+    //    private IUserAccountService UserAccountService;
 
-        public HomeController(IHomeService homeService, IUserAccountService userAccountService)
-        {
-            this.HomeService = homeService;
-            this.UserAccountService = userAccountService;
-        }
+    //    public HomeController(IHomeService homeService, IUserAccountService userAccountService)
+    //    {
+    //        this.HomeService = homeService;
+    //        this.UserAccountService = userAccountService;
+    //    }
 
-        public IActionResult Index()
-        {
-            if (this.User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("IndexLogged", "Home");
-            }
+    //    public IActionResult Index()
+    //    {
+    //        if (this.User.Identity.IsAuthenticated)
+    //        {
+    //            return RedirectToAction("IndexLogged", "Home");
+    //        }
 
-            return View();
-        }
+    //        return View();
+    //    }
 
-        [Authorize]
-        public IActionResult IndexLogged()
-        {
-            return View();
-        }
+    //    [Authorize]
+    //    public IActionResult IndexLogged()
+    //    {
+    //        return View();
+    //    }
 
-        public IActionResult About(AboutViewModel aboutViewModel)
-        {
-            aboutViewModel.Biography = this.HomeService.GetBiography();
+    //    public IActionResult About(AboutViewModel aboutViewModel)
+    //    {
+    //        aboutViewModel.Biography = this.HomeService.GetBiography();
 
-            return View(aboutViewModel);
-        }
+    //        return View(aboutViewModel);
+    //    }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
+    //    public IActionResult Contact()
+    //    {
+    //        return View();
+    //    }
 
-        [Authorize]
-        [HttpPost]
-        public IActionResult Contact(ContactMessageViewModel contactMessageViewModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return this.View(contactMessageViewModel);
-            }
-            string userId = null; 
+    //    [Authorize]
+    //    [HttpPost]
+    //    public IActionResult Contact(ContactMessageViewModel contactMessageViewModel)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return this.View(contactMessageViewModel);
+    //        }
+    //        string userId = null; 
 
-            if (User.Identity.IsAuthenticated)
-            {
-                userId = this.UserAccountService.GetCurrentUser(HttpContext).Id;
-            }
+    //        if (User.Identity.IsAuthenticated)
+    //        {
+    //            userId = this.UserAccountService.GetCurrentUser(HttpContext).Id;
+    //        }
 
-            this.HomeService.SaveContactMessage(contactMessageViewModel, userId);
+    //        var contactMessage = new ContactMessage()
+    //        { v};
+    //        this.HomeService.SaveContactMessage(contactMessageViewModel, userId);
 
-            return RedirectToAction("Success", "Home");
-        }
+    //        return RedirectToAction("Success", "Home");
+    //    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+    //    public IActionResult Privacy()
+    //    {
+    //        return View();
+    //    }
 
-        public IActionResult Success()
-        {
-            return View();
-        }
+    //    public IActionResult Success()
+    //    {
+    //        return View();
+    //    }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+    //    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //    public IActionResult Error()
+    //    {
+    //        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    //    }
+    //}
 }
--- Web Art Gallery/WAG WebApp/Areas/BG/Controllers/UserAccountController.cs
+++ Web Art Gallery/WAG WebApp/Areas/BG/Controllers/UserAccountController.cs
@@ -8,147 +8,147 @@ using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace WAG.WebApp.Areas.BG.Controllers
 {
-    public class UserAccountController : BGController
-    {
-        private const string UnsuccessfulRegistrationMessage = "This Username already exist! Please choose another one";
-        private const string UnsuccessfulLogInMessage = "Username or password is incorrect!";
+    //public class UserAccountController : BGController
+    //{
+    //    private const string UnsuccessfulRegistrationMessage = "This Username already exist! Please choose another one";
+    //    private const string UnsuccessfulLogInMessage = "Username or password is incorrect!";
 
-        private IUserAccountService UserAccountService;
+    //    private IUserAccountService UserAccountService;
 
-        public UserAccountController(IUserAccountService userAccountService)
-        {
-            this.UserAccountService = userAccountService;
-        }
+    //    public UserAccountController(IUserAccountService userAccountService)
+    //    {
+    //        this.UserAccountService = userAccountService;
+    //    }
 
-        public IActionResult Login()
-        {
-            if (this.User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("Index", "Home");
-            }
+    //    public IActionResult Login()
+    //    {
+    //        if (this.User.Identity.IsAuthenticated)
+    //        {
+    //            return RedirectToAction("Index", "Home");
+    //        }
 
-            return this.View();
-        }
+    //        return this.View();
+    //    }
 
-        [HttpPost]
-        public IActionResult Login(LoginInputViewModel loginInputModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return this.View(loginInputModel);
-            }
+    //    [HttpPost]
+    //    public IActionResult Login(LoginInputViewModel loginInputModel)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return this.View(loginInputModel);
+    //        }
 
-            var loginResult = this.UserAccountService.LoginUserSuccessfully(loginInputModel);
+    //        var loginResult = this.UserAccountService.LoginUserSuccessfully(loginInputModel);
 
-            if (loginResult != SignInResult.Success)
-            {
-                loginInputModel.UnsuccessfulLogInMessage = UnsuccessfulLogInMessage;
+    //        if (loginResult != SignInResult.Success)
+    //        {
+    //            loginInputModel.UnsuccessfulLogInMessage = UnsuccessfulLogInMessage;
 
-                return this.View(loginInputModel);
-            }
+    //            return this.View(loginInputModel);
+    //        }
 
-            return RedirectToAction("Index", "Home");
-        }
+    //        return RedirectToAction("Index", "Home");
+    //    }
 
-        public IActionResult Register()
-        {
-            if (this.User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction("Index", "Home");
-            }
+    //    public IActionResult Register()
+    //    {
+    //        if (this.User.Identity.IsAuthenticated)
+    //        {
+    //            return RedirectToAction("Index", "Home");
+    //        }
 
-            return this.View();
-        }
+    //        return this.View();
+    //    }
 
-        [HttpPost]
-        public IActionResult Register(RegisterInputViewModel registerInputViewModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return this.View(registerInputViewModel);
-            }
+    //    [HttpPost]
+    //    public IActionResult Register(RegisterInputViewModel registerInputViewModel)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return this.View(registerInputViewModel);
+    //        }
 
-            var registerResult = this.UserAccountService.CreateUserAsync(registerInputViewModel).Result;
+    //        var registerResult = this.UserAccountService.CreateUserAsync(registerInputViewModel).Result;
 
-            if (registerResult != IdentityResult.Success)
-            {
-                registerInputViewModel.UnsuccessfulRegistrationMessage = UnsuccessfulRegistrationMessage;
+    //        if (registerResult != IdentityResult.Success)
+    //        {
+    //            registerInputViewModel.UnsuccessfulRegistrationMessage = UnsuccessfulRegistrationMessage;
 
-                return this.View(registerInputViewModel);
-            }
+    //            return this.View(registerInputViewModel);
+    //        }
 
-            return RedirectToAction("Login", "UserAccount");
-        }
+    //        return RedirectToAction("Login", "UserAccount");
+    //    }
 
-        [Authorize]
-        public IActionResult Logout()
-        {
-            this.UserAccountService.Logout();
+    //    [Authorize]
+    //    public IActionResult Logout()
+    //    {
+    //        this.UserAccountService.Logout();
 
-            return RedirectToAction("Index", "Home");
-        }
+    //        return RedirectToAction("Index", "Home");
+    //    }
 
-        [Authorize]
-        public IActionResult MyProfile(UserDetailsViewModel userDetailsViewModel)
-        {
-            var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
+    //    [Authorize]
+    //    public IActionResult MyProfile(UserDetailsViewModel userDetailsViewModel)
+    //    {
+    //        var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
 
-            userDetailsViewModel.UserName = currUser.UserName;
-            userDetailsViewModel.FirstName = currUser.FirstName;
-            userDetailsViewModel.LastName = currUser.LastName;
-            userDetailsViewModel.City = currUser.City;
-            userDetailsViewModel.Address = currUser.Address;
-            userDetailsViewModel.Email = currUser.Email;
-            userDetailsViewModel.PhoneNumber = currUser.PhoneNumber;
-            userDetailsViewModel.IdentityRoles = this.UserAccountService.GetUserRolesNameById(currUser.Id).ToList();
+    //        userDetailsViewModel.UserName = currUser.UserName;
+    //        userDetailsViewModel.FirstName = currUser.FirstName;
+    //        userDetailsViewModel.LastName = currUser.LastName;
+    //        userDetailsViewModel.City = currUser.City;
+    //        userDetailsViewModel.Address = currUser.Address;
+    //        userDetailsViewModel.Email = currUser.Email;
+    //        userDetailsViewModel.PhoneNumber = currUser.PhoneNumber;
+    //        userDetailsViewModel.IdentityRoles = this.UserAccountService.GetUserRolesNameById(currUser.Id).ToList();
 
-            return this.View(userDetailsViewModel);
-        }
+    //        return this.View(userDetailsViewModel);
+    //    }
 
-        [Authorize]
-        public IActionResult EditUserProfile(EditUserProfileViewModel editUserProfileViewModel)
-        {
-            var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
+    //    [Authorize]
+    //    public IActionResult EditUserProfile(EditUserProfileViewModel editUserProfileViewModel)
+    //    {
+    //        var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
 
-            editUserProfileViewModel.FirstName = currUser.FirstName;
-            editUserProfileViewModel.LastName = currUser.LastName;
-            editUserProfileViewModel.City = currUser.City;
-            editUserProfileViewModel.Address = currUser.Address;
-            editUserProfileViewModel.Email = currUser.Email;
-            editUserProfileViewModel.PhoneNumber = currUser.PhoneNumber;
+    //        editUserProfileViewModel.FirstName = currUser.FirstName;
+    //        editUserProfileViewModel.LastName = currUser.LastName;
+    //        editUserProfileViewModel.City = currUser.City;
+    //        editUserProfileViewModel.Address = currUser.Address;
+    //        editUserProfileViewModel.Email = currUser.Email;
+    //        editUserProfileViewModel.PhoneNumber = currUser.PhoneNumber;
 
-            return this.View(editUserProfileViewModel);
-        }
+    //        return this.View(editUserProfileViewModel);
+    //    }
 
-        [Authorize]
-        [HttpPost]
-        public IActionResult EditUserProfile(EditUserProfileInputViewModel editUserProfileInputViewModel)
-        {
-            var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
+    //    [Authorize]
+    //    [HttpPost]
+    //    public IActionResult EditUserProfile(EditUserProfileInputViewModel editUserProfileInputViewModel)
+    //    {
+    //        var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
 
-            this.UserAccountService.EditUserProfile(currUser, editUserProfileInputViewModel);
+    //        this.UserAccountService.EditUserProfile(currUser, editUserProfileInputViewModel);
 
-            return RedirectToAction("Success", "Home");
-        }
+    //        return RedirectToAction("Success", "Home");
+    //    }
 
-        public IActionResult ChangePassword()
-        {
-            return this.View();
-        }
+    //    public IActionResult ChangePassword()
+    //    {
+    //        return this.View();
+    //    }
 
-        [HttpPost]
-        public IActionResult ChangePassword(ChangePasswordViewModel changePasswordViewModel)
-        {
-            if (!ModelState.IsValid)
-            {
-                return this.View(changePasswordViewModel);
-            }
+    //    [HttpPost]
+    //    public IActionResult ChangePassword(ChangePasswordViewModel changePasswordViewModel)
+    //    {
+    //        if (!ModelState.IsValid)
+    //        {
+    //            return this.View(changePasswordViewModel);
+    //        }
 
-            var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
+    //        var currUser = this.UserAccountService.GetCurrentUser(HttpContext);
 
-            this.UserAccountService.ChangePassword(currUser, changePasswordViewModel.CurrPassword, changePasswordViewModel.NewPassword);
+    //        this.UserAccountService.ChangePassword(currUser, changePasswordViewModel.CurrPassword, changePasswordViewModel.NewPassword);
             
-            return RedirectToAction("Success", "Home");
-        }
-    }
+    //        return RedirectToAction("Success", "Home");
+    //    }
+    //}
 }
